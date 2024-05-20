c96c86db1014a6289956f84c006c43b8e3c3ca4f
alekseybulanov <aleksey.bulanov1@gmail.com>
2024-05-20 13:38:09 +0300

    add reactive controls for stories

--- src/composables/widgets/settings.ts
+++ src/composables/widgets/settings.ts
@@ -1,4 +1,4 @@
-import { ref, type Ref } from "vue";
+import { ref, type Ref, watch } from "vue";
 
 export function useSettings<Type>(props: any) {
   const settings = ref({}) as Ref<Type>;
@@ -11,6 +11,15 @@ export function useSettings<Type>(props: any) {
     settings.value[key] = value;
   };
 
+  watch(
+    props,
+    () => {
+      Object.keys(props).forEach((key) => {
+        settings.value[key] = props[key];
+      });
+    }
+  )
+  
   return {
     settings,
     setSetting,
